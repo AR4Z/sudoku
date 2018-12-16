7bd9de7dba9339c196c0767f2f50a538cde37371
Orlando Hernandez <oaraz02@gmail.com>
2018-12-16 10:37:33 -0500

    folder components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import Board from './Board';
+import Board from './components/Board';
 
 class App extends Component<{}, {}> {
   render() {
